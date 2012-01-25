01479529336c1890395ff416b6317dd74307c1b9
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2012-01-25 10:11:24 +0100

    Added xscreensaver -lock in awesome

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -194,6 +194,7 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
+    awful.key({ "Control", "Mod1" }, "l",      function () awful.util.spawn("xscreensaver-command -lock") end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
