d654d43b68d971fe3423c9ada41409b60fde7db3
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2012-01-25 14:56:34 +0100

    xterm -> urxvt

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -13,7 +13,7 @@ require("naughty")
 beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "uxterm"
+terminal = "urxvtc"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
