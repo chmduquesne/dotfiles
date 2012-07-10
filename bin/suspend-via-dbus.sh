d4f0d2aa9c990720de8d112ed39422995f49ea72
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2012-07-10 11:59:41 +0200

    removed legacy scripts, adding automatic backups

--- bin/suspend-via-dbus.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-gnome-screensaver-command -l
-sleep 2
-dbus-send --system --print-reply --dest=org.freedesktop.Hal /org/freedesktop/Hal/devices/computer org.freedesktop.Hal.Device.SystemPowerManagement.Suspend int32:0
